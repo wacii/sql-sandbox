f7612eb1f3092b4970592583c5f010d4c895435a
William Cunningham <w.a.cunningham.ii@gmail.com>
2016-01-30 17:36:51 -0500

    decouple command history from dom

--- src/command-history.js
+++ src/command-history.js
@@ -1,13 +1,19 @@
-'use strict';
-
+const EventEmitter = require('events');
 const push = Array.prototype.push;
 const splice = Array.prototype.splice;
 
-// remember user commands so they can be browsed with up and down arrow
-class CommandHistory {
-  constructor(el) {
-    this.input = el;
+// Remember user commands so they can be browsed with up and down arrow.
+//
+// `push()` new commands onto history instance. Navigate with `back()` and
+// `forward()`. Get current `command`, or listen for `'change'`s.
+//
+class CommandHistory extends EventEmitter {
+  constructor() {
     this.pointer = 0;
+  }
+
+  get command() {
+    return this[this.pointer];
   }
 
   push(value) {
@@ -17,17 +23,18 @@ class CommandHistory {
 
   back() {
     if (this.pointer > 0) this.pointer--;
-    this.input.val(this[this.pointer]);
+    this.emit('change', this.command);
   }
 
   forward() {
     if (this.pointer < this.length) this.pointer++;
-    this.input.val(this[this.pointer]);
+    this.emit('change', this.command);
   }
 
   clear() {
     splice.call(this, 0, this.length);
     this.pointer = 0;
+    this.emit('change', '');
   }
 }
 
