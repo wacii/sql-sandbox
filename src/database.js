38c7e863c8ab105b8b445a80e013d175fdeebde5
William Cunningham <w.a.cunningham.ii@gmail.com>
2016-01-30 17:21:48 -0500

    extract database class

---
+++ src/database.js
@@ -0,0 +1,41 @@
+const SQL = SQL; // FIXME
+const resultsTemplate = require('./templates').resultsTemplate;
+function noop() {} // TODO: extract somewhere
+
+// NOTE: consider using error template for error message and just
+//   adding the connection class to the container <li> in terminal
+
+// Encapsulate creation and normal use of a database.
+//
+module.exports = class Database {
+  constructor() {
+    this.db = new SQL.Database();
+  }
+
+  // Evaluate a command, with callbacks to handle results and errors.
+  //
+  // onResults(results, html)
+  // onError(error, html)
+  //
+  // db.evaluate('SELECT * FROM products', logResults, logError);
+  evaluate(command, onResults = noop, onError = noop) {
+    let statement;
+    try {
+      statement = this.db.prepare(command);
+      if (statement.step()) {
+        const results = this.db.exec(command)[0];
+        onResults(results, resultsTemplate(results));
+      }
+    } catch (error) {
+      onError(error, error);
+    } finally {
+      statement.free();
+      return command;
+    }
+  }
+
+  reset() {
+    this.db.close();
+    this.db = new SQL.Database();
+  }
+};
